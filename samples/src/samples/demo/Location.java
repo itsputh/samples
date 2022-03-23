4296d0c6d766927d2ffa77084664e680f4a7a091
itsputh <puteribalkis010107@gmail.com>
2022-03-24 00:08:39 +0800

    Third Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,36 @@
+package samples.demo;
+
+public class Location {
+	
+	private String name;
+	private int age;
+	private int year;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
